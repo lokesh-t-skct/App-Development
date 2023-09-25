f50c92a6e0fa4e406ab33e3d60a46f0724d24a6b
lokesh-t-skct <126309274+lokesh-t-skct@users.noreply.github.com>
2023-09-25 14:25:49 +0530

    Add files via upload

---
+++ day 9/src/PAGES/Homepage.js
@@ -0,0 +1,90 @@
+// src/components/Home.js
+import React from 'react';
+import Footer from '../COMPONENTS/footer';
+import Header from '../COMPONENTS/header';
+import Tabs from '../COMPONENTS/tabs';
+import '../Stylee/Home.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+
+const Home = () => {
+    
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Breakfast',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
+      title: 'Burger',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+      title: 'Camera',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+      title: 'Coffee',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
+      title: 'Hats',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
+      title: 'Honey',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+      title: 'Basketball',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
+      title: 'Fern',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+      title: 'Mushrooms',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+      title: 'Tomato basil',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
+      title: 'Sea star',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
+      title: 'Bike',
+    },
+  ];
+  return (
+    <div className="home">
+    <Header/>
+    <Tabs/>
+      <h2>Welcome to Our Home Design and Renovations Website</h2>
+      <p>
+        We specialize in turning your dream home into reality. Our team of
+        experts is dedicated to providing top-notch services, from kitchen
+        remodeling to bathroom renovations.
+      </p>
+      <ImageList sx={{ width: 1300, height: 450 }} cols={5} rowHeight={164}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img}>
+          <img
+            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+    <Footer/>
+    </div>
+  );
+};
+
+export default Home;
